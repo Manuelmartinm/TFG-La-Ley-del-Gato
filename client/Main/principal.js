88c6177b4ec6355e05f5386c836bf10e2e6711eb
ManuelRM-Medac <mrm0045@alu.medac.es>
2026-03-19 12:55:23 +0100

    pagina principal

---
+++ client/Main/principal.js
@@ -0,0 +1,7 @@
+    window.onload = function () {
+            // Carga el nombre y avatar del agente desde localStorage (guardado al registrarse)
+        const nombre = localStorage.getItem('nombre_usuario') || 'AGENTE_01';
+        const avatar = localStorage.getItem('avatar') || '🐭';
+        document.getElementById('agentName').textContent = nombre.toUpperCase();
+        document.getElementById('agentAvatar').textContent = avatar;
+    }
--- server/index.js
+++ server/index.js
@@ -27,5 +27,5 @@ app.get('/', (req, res) => {
 // Arranca el servidor en el puerto definido en .env o en el 3000 por defecto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log('🚀 Servidor corriendo en el puerto ${PORT}');
 });
